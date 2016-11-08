b3283ad812a2eb4cfd10d995a4a7f4f40c141bb8
macdt <macdt@macdts-MacBook-Pro.local>
2016-11-08 13:17:20 +0100

    improve version, repair build

--- ios_lab2/AlbumsTableViewController.swift
+++ ios_lab2/AlbumsTableViewController.swift
@@ -9,32 +9,15 @@
 import UIKit
 
 class AlbumsTableViewController: UITableViewController {
-
-    var albums = [Album]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-      var albums = [Album]()
-        
-        let album = Album()
-        album.title = "x"
-        
-        albums.append(album)
-        
-        let album2 = Album()
-        album2.title = "y"
-        albums.append(album2)
 
-    
-        AlbumSingleton.sharedInstance.Albums = albums
-        
-        tableView.reloadData()
     }
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
-        tableView.reloadData()
+//        tableView.reloadData()
     }
     
     override func didReceiveMemoryWarning() {
@@ -49,16 +32,16 @@ class AlbumsTableViewController: UITableViewController {
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    
-        return albums.count
+    return 0
+//        return albums.count
     }
 
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("cellID", forIndexPath: indexPath)
 
-        cell.textLabel?.text = albums[indexPath.row]
-        cell.detailTextLabel?.text = "a"
+//        cell.textLabel?.text = albums[indexPath.row]
+//        cell.detailTextLabel?.text = "a"
 
         return cell
     }
@@ -71,11 +54,11 @@ class AlbumsTableViewController: UITableViewController {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
         
-        if let vc = segue.destinationViewController as? ViewController{
-            if let cell = sender as? UITableViewCell {
-                let index = tableView.indexPathForCell(cell)
-                    vc.album = albums[(index?.row)!]
-            }
-        }
+//        if let vc = segue.destinationViewController as? ViewController{
+//            if let cell = sender as? UITableViewCell {
+//                let index = tableView.indexPathForCell(cell)
+//                    vc.album = albums[(index?.row)!]
+//            }
+//        }
     }
 }
--- ios_lab2/ViewController.swift
+++ ios_lab2/ViewController.swift
@@ -10,53 +10,57 @@ import UIKit
 
 class ViewController: UIViewController {
     // MARK: Properties
-    //    @IBOutlet weak var artistTextField: NSLayoutConstraint!
-    //    @IBOutlet weak var titleTextField: UITextField!
-    //    @IBOutlet weak var genreTextField: UITextField!
-    //    @IBOutlet weak var yearTextView: UITextField!
-    //
-    //    @IBOutlet weak var numberRate: UILabel!
-    //
-    //    @IBOutlet weak var buttonRates: UIStepper!
-    //    @IBOutlet weak var buttonPrev: UIButton!
-    //    @IBOutlet weak var buttonNext: UIButton!
-    //    @IBOutlet weak var buttonSave: UIButton!
-    //    @IBOutlet weak var buttonDelete: UIButton!
-    //    @IBOutlet weak var buttonNew: UIButton!
-    //
-    //    @IBOutlet weak var numberRecord: UITextView!
-    
-    //    var albums: NSMutableArray?
-    
-    
-    var albums = ""
-    var albumIndex = AlbumSingleton.sharedInstance.Albums
-    
-    @IBOutlet weak var textFieldTitle: UITextField!
+    @IBOutlet weak var artistTextField: UITextField!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var genreTextField: UITextField!
+    @IBOutlet weak var yearTextView: UITextField!
     
     @IBOutlet weak var stepper: UIStepper!
     @IBOutlet weak var labelRating: UILabel!
     @IBOutlet weak var buttonSave: UIButton!
+    @IBOutlet weak var buttonDelete: UIButton!
+    @IBOutlet weak var buttonCancel: UIButton!
     
-    let
+    let plistCatPath=NSBundle.mainBundle().pathForResource("Albums", ofType: "plist")
+    let documentsPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0]
+    let fileManager = NSFileManager.defaultManager()
+    
+    var albums: NSMutableArray=[]
+    var albumsDocPath: String = ""
+    
+    var record:[String:AnyObject]=[:]
+    var currentRecord: Int = 0
+    
+    deinit{
+        writeFile()
+    }
+    
+    func writeFile(){
+        albums.writeToFile(albumsDocPath, atomically: true)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        albums = AlbumSingleton.sharedInstance.Albums
+        albumsDocPath = documentsPath.stringByAppendingString("/Albums.plist")
+        if !fileManager.fileExistsAtPath(albumsDocPath){
+            try? fileManager.copyItemAtPath(plistCatPath!, toPath: albumsDocPath)
+        }
         
+        buttonSave.enabled=false;
+        albums = readFile()
         
-        textFieldTitle.text = albums[albumIndex].title
-        buttonSave.enabled = false
+        artistTextField.text=albums[currentRecord].valueForKey("artist") as? String
+        titleTextField.text=albums[currentRecord].valueForKey("title") as? String
+        genreTextField.text=albums[currentRecord].valueForKey("genre") as? String
         
-        //
-        //        albums = NSMutableArray(contentsOfFile:"Albums");
-        // Do any additional setup after loading the view, typically from a nib.
-        //        let album = albums[0]
-        
-        //        artistTextField.text = album["artist"] as? String
+        labelRating.text=String(albums[currentRecord].valueForKey("rating") as! Int)
     }
     
+    func readFile()->NSMutableArray{
+        return NSMutableArray(contentsOfFile:albumsDocPath)!
+    }
+
     // MARK: Actions
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -67,12 +71,101 @@ class ViewController: UIViewController {
         labelRating.text = "\(stepper.value)"
     }
     
-    @IBAction func saveClicked(sender: AnyObject) {
-        AlbumSingleton.sharedInstance.Albums[albumIndex].title = textFieldTitle.text!
+    
+    func getCur(index:Int)->Int{
+        if index < 0 {
+            return 0
+        }
+        if index > albums.count-1{
+            return albums.count - 1
+        }
+        return index as Int
+    }
+    
+    @IBAction func clickBtnSave(sender: AnyObject) {
+        let artistR = artistTextField.text ?? ""
+        let genreR = genreTextField.text ?? ""
+        let titleR = titleTextField.text ?? ""
+        let yearR = Int(yearTextView.text!) ?? 0
+        let ratingR = Int(labelRating.text!) ?? 0
+        if currentRecord==albums.count{
+            albums.addObject(["artist":artistR,"title":titleR,"genre":genreR,"date":yearR,"rating":ratingR])
+        }
+        
+        if currentRecord >= 0 && currentRecord < albums.count {
+            albums.replaceObjectAtIndex(currentRecord, withObject: ["artist":artistR,"title":titleR,"genre":genreR,"date":yearR,"rating":ratingR])
+        }
+    }
+    
+    @IBAction func clickBtnDelete(sender: UIButton) {
+        if albums.count == 1 {
+            
+            albums.removeObjectAtIndex(0)
+            currentRecord=getCur(currentRecord-1)
+            titleTextField.text=""
+            genreTextField.text=""
+            yearTextView.text=String(0)
+            artistTextField.text=""
+            labelRating.text=String(0)
+        } else  if currentRecord < albums.count{
+            albums.removeObjectAtIndex(currentRecord)
+            currentRecord=getCur(currentRecord-1)
+            showRecord(currentRecord)
+        }
+    }
+
+    @IBAction func clickBtnCancel(sender: AnyObject) {
+        //TODO: return to TableView
+    }
+    
+    func showRecord(index:Int)->Void{
+        artistTextField.text=albums[index].valueForKey("artist") as? String
+        titleTextField.text=albums[index].valueForKey("title") as? String
+        genreTextField.text=albums[index].valueForKey("genre") as? String
+        
+        labelRating.text=String(albums[index].valueForKey("rating") as! Int)
+        yearTextView.text=String(albums[index].valueForKey("date") as! Int)
+    }
+    
+    @IBAction func changeStepperRate(sender: UIStepper) {
+        if  sender.value>5{
+            sender.value=5
+        }
+        
+        if sender.value<0{
+            sender.value=0
+        }
+        buttonSave.enabled=true
+        labelRating.text=String(Int(sender.value ))
     }
     
     @IBAction func anyProportyOfAlbumChanged(sender: AnyObject) {
         buttonSave.enabled = true
+    }
+    
+    @IBAction func changeTxtYear(sender: UITextField) {
+        buttonSave.enabled=true
+    }
+    
+    @IBAction func changeTxtGenre(sender: UITextField) {
+        buttonSave.enabled=true
+    }
+    
+    @IBAction func changeTxtTitle(sender: UITextField) {
+        buttonSave.enabled=true
+    }
+    
+    @IBAction func changeTxtArtist(sender: AnyObject) {
+        buttonSave.enabled=true
+    }
+    
+    func exit(){
+        albums.writeToFile(albumsDocPath, atomically: true)
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?){
+        view.endEditing(true)
+        super.touchesBegan(touches, withEvent: event)
     }
 }
 
